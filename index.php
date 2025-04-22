a698d08ccf778e9f8e3677cc48f6b001cd89e9e9
conghyu51 <conghyu51@gmail.com>
2025-04-22 19:36:04 +0700

    refactor

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php
 
-require_once __DIR__ . '/vendor/autoload.php';
+use Symfony\Component\Dotenv\Dotenv;
+
+define('BASE_PATH', __DIR__);
+
+require_once BASE_PATH . '/vendor/autoload.php';
+
+$dotenv = new Dotenv();
+$dotenv->load(BASE_PATH . '/.env');
